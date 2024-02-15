32fd5a8ca99400adaa39f5fdfc60bef1c04491b1
gabo482 <vcruztesci@gmail.com>
2024-02-15 11:23:21 -0600

    Se reestructura contenidos del curso

--- Lenguaje manipulacion de datos/Uso de FUNCION CONCAT.sql
+++ Lenguaje manipulacion de datos/Uso de FUNCION CONCAT.sql
@@ -6,3 +6,8 @@ FROM curso_intersemestral.alumnos AS A
 JOIN curso_intersemestral.localidad AS L  
 ON (A.id_localidad= L.id_localidad) 
 ORDER BY localidad ASC;
+
+
+SELECT A.NOMBRE , L.localidad from curso_intersemestral.alumnos AS A , curso_intersemestral.localidad AS L 
+WHERE A.id_localidad= L.id_localidad ;
+

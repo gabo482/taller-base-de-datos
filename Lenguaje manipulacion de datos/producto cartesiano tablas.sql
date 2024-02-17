9b9d3482e0ce22697d770973416a290fc0c171d5
Víctor Gabriel Cruz Arrona <vcruztesci@gmail.com>
2024-02-17 09:12:05 -0600

    Update producto cartesiano tablas.sql
    se corrige comentario

--- Lenguaje manipulacion de datos/producto cartesiano tablas.sql
+++ Lenguaje manipulacion de datos/producto cartesiano tablas.sql
@@ -1,7 +1,7 @@
 
 /*
 Ejemplo de consulta con el uso de producto cartesiano, siempre se recomienda realizar uso de JOIN,
-sin embargo se ejemplifica que se puede llegar a encontar este tipo de consultas sin el uso de JOIN
+sin embargo se ejemplifica que se puede llegar a encontrar este tipo de consultas sin el uso de JOIN
 */
 
 SELECT A.NOMBRE , L.localidad from curso_intersemestral.alumnos AS A , curso_intersemestral.localidad AS L 
